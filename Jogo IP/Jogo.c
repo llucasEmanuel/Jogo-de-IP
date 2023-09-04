5480dd24642b17fd676daa9486f8e7d4121924c9
llucasEmanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-09-04 09:48:59 -0300

    Update Jogo.c

--- Jogo IP/Jogo.c
+++ Jogo IP/Jogo.c
@@ -4,6 +4,9 @@
 #include <math.h>
 #include <time.h>
 #include <raylib.h>
+#include "menu.h"
+
+#define PI 3.14159265
 
 //CRIAR BIBLIOTECAS ESPECIFICAS PARA PLAYER, INIMIGOS, COLETAVEIS, ...
 typedef struct {
@@ -13,6 +16,7 @@ typedef struct {
     int temChave;//flag que indica se tem (1) ou nao (0) a chave
     int qtdBaterias;//qtd de baterias coletadas pelo jogador na fase
     int score;
+    float campoVisao;//circulo de luz gerado pela lanterna (varia em funcao do jogador.qtdBaterias)
 } Player;
 
 typedef struct {
@@ -39,6 +43,7 @@ Player inicializarJogador(Window janela);
 Collectable inicializarChave(Window janela);
 Collectable *inicializarBaterias(Window janela, int *qtdBaterias);
 Collectable inicializarPorta(Window janela);
+Enemy *inicializarInimigos(Window janela);
 //Criar funcoes de inicializar jogador, baterias e chave e colocar na de inicializar fase
 //Depois criar a funcao de inicializar fase, que add inimigos e reseta os objetos e o jogador
 int main() {
@@ -50,6 +55,8 @@ int main() {
     janela.width = GetScreenWidth();
     //INICIALIZACAO DO JOGADOR
     Player jogador = inicializarJogador(janela);
+    Enemy *inimigos = NULL;
+    int qtdInimigos = 0;
     criarFase(&jogador, janela);
 
     return 0; 
@@ -57,8 +64,14 @@ int main() {
 
 void criarFase(Player *jogador, Window janela) {
     
+    //AUXILIAR MENU
+    int continua = 0, continua1 = 0;
+    
     InitAudioDevice();
     //if (!IsWindowFullscreen()) ToggleFullscreen();
+    
+    //INICIALIZACAO DOS INIMIGOS
+    Enemy *inimigos = inicializarInimigos(janela);
     
     //INICIALIZACAO DAS BATERIAS
     int qtdBaterias = 0;//qtd baterias que irao aparecer na tela (a qtd eh aleatorio e definida na funcao)
@@ -83,120 +96,140 @@ void criarFase(Player *jogador, Window janela) {
 
     //INICIO DO LOOP EM QUE RODA O JOGO
     while(!WindowShouldClose()) {
+                
         
-        while (entrouNaPorta) {
-            BeginDrawing();
-            ClearBackground(WHITE);
+        if(continua == 0 && continua1 == 0){ //Chama a primeira parte do menu
+           continua = geraMenu();
+       }
+       
+       if(continua == 1 && continua1 == 0){ //Chama a parte dos comandos
+           continua1 = iniciaJogo();
+       }
+       
+        
+        if(continua1 == 1 && continua == 1){ //Inicia Jogo
+                while (entrouNaPorta) {
+                    BeginDrawing();
+                    ClearBackground(WHITE);
 
-            DrawText(TextFormat("HIGHSCORE : %d", jogador->score), janela.width / 2 - MeasureText("HIGHSCORE : ", 75) / 2, janela.height / 2 - 75, 75, BLACK);
+                    DrawText(TextFormat("HIGHSCORE : %d", jogador->score), janela.width / 2 - MeasureText("HIGHSCORE : ", 75) / 2, janela.height / 2 - 75, 75, BLACK);
 
-            EndDrawing();
+                    EndDrawing();
+                    
+                    if (IsKeyPressed(KEY_ESCAPE)) {//mudar a tecla para KEY_SPACE depois
+                        entrouNaPorta = 0;
+                    }
+                }
             
-            if (IsKeyPressed(KEY_ESCAPE)) {//mudar a tecla para KEY_SPACE depois
-                entrouNaPorta = 0;
+            
+            BeginDrawing();
+            
+            // jogador->campoVisao = 150 + (25 * jogador->qtdBaterias);
+            // DrawCircle(jogador->hitbox.x + 46, jogador->hitbox.y + 40, jogador->campoVisao, WHITE);//campo de visao
+            ClearBackground(WHITE);
+     
+            DrawText("FASE 1", janela.width / 2 - MeasureText("FASE 1", 40) / 2, janela.height / 2 - 40, 40, BLACK);
+            
+            //DESENHA INIMIGOS
+            DrawTextureEx(inimigos[0].textura, inimigos[0].coordenadas, 0, 8, WHITE);
+            DrawRectangle(inimigos[0].hitbox.x, inimigos[0].hitbox.y, inimigos[0].hitbox.width, inimigos[0].hitbox.height, BLANK);
+            
+            //DESENHA A TEXTURA DAS BATERIAS 
+            for (int i = 0; i < qtdBaterias; i++) {
+                if (baterias[i].colisao == 0) {
+                    DrawTextureEx(baterias[i].textura, (Vector2) {baterias[i].coordenadas.x, baterias[i].coordenadas.y}, 0, 3, WHITE);
+                    DrawRectangle(baterias[i].hitbox.x, baterias[i].hitbox.y, baterias[i].hitbox.width, baterias[i].hitbox.height, BLANK);
+                }
             }
-        }
-        
-        BeginDrawing();
-        
-        ClearBackground(WHITE);
- 
-        DrawText("FASE 1", janela.width / 2 - MeasureText("FASE 1", 40) / 2, janela.height / 2 - 40, 40, BLACK);
-        
-        //DESENHA A TEXTURA DAS BATERIAS 
-        for (int i = 0; i < qtdBaterias; i++) {
-            if (baterias[i].colisao == 0) {
-                DrawTextureEx(baterias[i].textura, (Vector2) {baterias[i].coordenadas.x, baterias[i].coordenadas.y}, 0, 3, WHITE);
-                DrawRectangle(baterias[i].hitbox.x, baterias[i].hitbox.y, baterias[i].hitbox.width, baterias[i].hitbox.height, BLANK);
+            
+            //DESENHA A TEXTURA DA CHAVE
+            DrawTextureEx(chave.textura, (Vector2) {chave.coordenadas.x, chave.coordenadas.y}, 0, 2.5, WHITE);
+            DrawRectangle(chave.hitbox.x, chave.hitbox.y, chave.hitbox.width, chave.hitbox.height, BLANK);
+            
+            //hitbox dinamica que se move conforme o sprite do jogador
+            jogador->hitbox = (Rectangle) {
+                jogador->coordenadas.x + 18,
+                jogador->coordenadas.y + 40,
+                96,
+                90,
+            };
+            
+            if (!jogador->temChave) {//desenha antes se o jogador nao tiver a chave
+                DrawTextureEx(porta.textura, porta.coordenadas, 0, 2.2, WHITE);
+                DrawRectangle(porta.hitbox.x, porta.hitbox.y, porta.hitbox.width, porta.hitbox.height, BLANK);
             }
-        }
-        
-        //DESENHA A TEXTURA DA CHAVE
-        DrawTextureEx(chave.textura, (Vector2) {chave.coordenadas.x, chave.coordenadas.y}, 0, 2.5, WHITE);
-        DrawRectangle(chave.hitbox.x, chave.hitbox.y, chave.hitbox.width, chave.hitbox.height, BLANK);
-        
-        jogador->hitbox = (Rectangle) {//hitbox dinamica que se move conforme o sprite do jogador
-            jogador->coordenadas.x + 18,
-            jogador->coordenadas.y + 40,
-            96,
-            90,
-        };
-        
-        if (!jogador->temChave) {//desenha antes se o jogador nao tiver a chave
-            DrawTextureEx(porta.textura, porta.coordenadas, 0, 2.2, WHITE);
-            DrawRectangle(porta.hitbox.x, porta.hitbox.y, porta.hitbox.width, porta.hitbox.height, BLANK);
-        }
-        
-        //DESENHA A TEXTURA DO JOGADOR
-        DrawTextureEx(jogador->textura, jogador->coordenadas, 0, 0.33f, WHITE);
-        DrawRectangle(jogador->hitbox.x, jogador->hitbox.y, jogador->hitbox.width, jogador->hitbox.height, BLANK);
-        
-        if (jogador->temChave) {//desenha depois se tiver a chave
-            DrawTextureEx(porta.textura, porta.coordenadas, 0, 2.2, WHITE);
-            DrawRectangle(porta.hitbox.x, porta.hitbox.y, porta.hitbox.width, porta.hitbox.height, BLANK);
-        }
+            
+            //DESENHA A TEXTURA DO JOGADOR
+            DrawTextureEx(jogador->textura, jogador->coordenadas, 0, 0.33f, WHITE);
+            DrawRectangle(jogador->hitbox.x, jogador->hitbox.y, jogador->hitbox.width, jogador->hitbox.height, BLANK);
+            
+            if (jogador->temChave) {//desenha depois se tiver a chave
+                DrawTextureEx(porta.textura, porta.coordenadas, 0, 2.2, WHITE);
+                DrawRectangle(porta.hitbox.x, porta.hitbox.y, porta.hitbox.width, porta.hitbox.height, BLANK);
+            }
 
-        moverJogador(jogador);
-        
-        //CHECA SE HOUVE COLISAO COM A CHAVE
-        if (CheckCollisionRecs(jogador->hitbox, chave.hitbox)) {
-            chave.colisao = 1;
-            printf("Colisao CHAVE\n");
-            UnloadTexture(chave.textura);//apaga a textura
-            chave.hitbox = (Rectangle) {0, 0, 0, 0};//remove a hitbox
-            jogador->temChave = 1;//atualiza a flag do jogador
-            chavesStr[9]++;//atualiza a qtd chaves na tela
-            jogador->score += 200;
-        }
-        
-        //CHECA SE HOUVE COLISAO COM A PORTA
-        if (CheckCollisionRecs(jogador->hitbox, porta.hitbox)) {
-            if (jogador->temChave) {
-                porta.colisao = 1;
-                printf("Colisao PORTA\n");
-                UnloadTexture(porta.textura);
-                porta.hitbox = (Rectangle) {0, 0 , 0 , 0};
-                jogador->score += 500;
-                entrouNaPorta = 1;
-                //return;//Sai da funcao
+            moverJogador(jogador);
+            
+            //CHECA SE HOUVE COLISAO COM A CHAVE
+            if (CheckCollisionRecs(jogador->hitbox, chave.hitbox)) {
+                chave.colisao = 1;
+                printf("Colisao CHAVE\n");
+                UnloadTexture(chave.textura);//apaga a textura
+                chave.hitbox = (Rectangle) {0, 0, 0, 0};//remove a hitbox
+                jogador->temChave = 1;//atualiza a flag do jogador
+                chavesStr[9]++;//atualiza a qtd chaves na tela
+                jogador->score += 200;
             }
-            else {
-                DrawText("PORTA TRANCADA", janela.width/2 - MeasureText("PORTA TRANCADA", 30), 30, 40, RED);
+            
+            //CHECA SE HOUVE COLISAO COM A PORTA
+            if (CheckCollisionRecs(jogador->hitbox, porta.hitbox)) {
+                if (jogador->temChave) {
+                    porta.colisao = 1;
+                    printf("Colisao PORTA\n");
+                    UnloadTexture(porta.textura);
+                    porta.hitbox = (Rectangle) {0, 0 , 0 , 0};
+                    jogador->score += 500;
+                    entrouNaPorta = 1;
+                    //return;//Sai da funcao
+                }
+                else {
+                    DrawText("PORTA TRANCADA", janela.width/2 - MeasureText("PORTA TRANCADA", 30), 30, 40, RED);
+                }
             }
-        }
-        
-        //CHECA SE HOUVE COLISAO COM UMA DAS BATERIAS
-        int indColisoes[qtdBaterias];//armazena os indices das baterias colididas
-        int qtdColisoes = 0;//qtd de baterias obtidas
-        for (int i = 0; i < qtdBaterias; i++) {
-            if (CheckCollisionRecs(jogador->hitbox, baterias[i].hitbox)) {
-                indColisoes[qtdColisoes] = i;//guarda os indices
-                qtdColisoes++;
-                bateriasStr[11]++;//atualiza a qtd baterias na tela
+            
+            //CHECA SE HOUVE COLISAO COM UMA DAS BATERIAS
+            int indColisoes[qtdBaterias];//armazena os indices das baterias colididas
+            int qtdColisoes = 0;//qtd de baterias obtidas
+            for (int i = 0; i < qtdBaterias; i++) {
+                if (CheckCollisionRecs(jogador->hitbox, baterias[i].hitbox)) {
+                    indColisoes[qtdColisoes] = i;//guarda os indices
+                    qtdColisoes++;
+                    bateriasStr[11]++;//atualiza a qtd baterias na tela
+                }
             }
-        }
-        
-        //REMOVER BATERIAS
-        for (int i = 0 ; i < qtdColisoes; i++) { 
-            int ind = indColisoes[i];
-            //checa a flag da bateria daquele indice, se o indice estiver dentro da lista e flag for 0,
-            //entao tem que apagar a bateria
-            if (baterias[ind].colisao == 0) {
-                baterias[ind].colisao = 1;
-                printf("Colisao BATERIA\n");
-                UnloadTexture(baterias[ind].textura);
-                baterias[ind].hitbox = (Rectangle){0, 0, 0, 0};
-                jogador->qtdBaterias++; 
-                jogador->score+= 50;
+            
+            //REMOVER BATERIAS
+            for (int i = 0 ; i < qtdColisoes; i++) { 
+                int ind = indColisoes[i];
+                //checa a flag da bateria daquele indice, se o indice estiver dentro da lista e flag for 0,
+                //entao tem que apagar a bateria
+                if (baterias[ind].colisao == 0) {
+                    baterias[ind].colisao = 1;
+                    printf("Colisao BATERIA\n");
+                    UnloadTexture(baterias[ind].textura);
+                    baterias[ind].hitbox = (Rectangle){0, 0, 0, 0};
+                    jogador->qtdBaterias++; 
+                    jogador->score += 50;
+                }
             }
-        }
 
-        //GUARDAR CADA STRING EM UMA VARIAVEL DIFERENTE E SÓ SOMAR 1 NO CHAR DO DIGITO
-        DrawText(TextFormat("SCORE : %d", jogador->score), 30, 30, 40, BLACK);
-        DrawText(chavesStr, 30, 80, 40, BLACK);
-        DrawText(bateriasStr, 30, 130, 40, BLACK);
+            //GUARDAR CADA STRING EM UMA VARIAVEL DIFERENTE E SÓ SOMAR 1 NO CHAR DO DIGITO
+            DrawText(TextFormat("SCORE : %d", jogador->score), 30, 30, 40, GRAY);
+            DrawText(chavesStr, 30, 80, 40, GRAY);
+            DrawText(bateriasStr, 30, 130, 40, GRAY);
 
-        EndDrawing();        
+            EndDrawing();  
+        }
     }
 
     //DESALOCA OS PONTEIROS, APAGA AS TEXTURAS E DESATIVA AS FUNCOES DE AUDIO
@@ -218,6 +251,7 @@ Player inicializarJogador(Window janela) {
     jogador.textura = LoadTexture("Sprites e Texturas/sprite1.png");
     jogador.coordenadas = (Vector2) {janela.width/2, janela.height/2};
     jogador.score = 0;
+    //jogador.campoVisao = 150 + (50 * jogador.qtdBaterias);//raio = 1;
     return jogador;
 }
 
@@ -268,7 +302,7 @@ Collectable inicializarChave(Window janela) {
 Collectable inicializarPorta(Window janela) {
     Collectable porta;
     porta.textura = LoadTexture("Sprites e Texturas/porta.png");
-    porta.coordenadas = (Vector2) {(float) (rand() % (janela.width - 36)), (float) (rand() % (janela.height - 55))};
+    porta.coordenadas = (Vector2) {(float) (rand() % (janela.width - (porta.textura.width * 2))), (float) (rand() % (janela.height - (porta.textura.height * 2)))};
     porta.hitbox = (Rectangle) {
         porta.coordenadas.x + 31,
         porta.coordenadas.y + 50,
@@ -279,57 +313,75 @@ Collectable inicializarPorta(Window janela) {
     return porta;
 }
 
+Enemy *inicializarInimigos(Window janela) {
+    Enemy *ptrAux = (Enemy *) malloc(sizeof(Enemy));
+    if (ptrAux == NULL) {
+        printf("Problema de alocacao de memoria.\n");
+        exit(1);
+    }
+    Enemy *inimigos = ptrAux;
+    inimigos[0].textura = LoadTexture("Sprites e Texturas/saltyPaul.png");
+    inimigos[0].coordenadas = (Vector2) {(float) (rand() % (janela.width - (inimigos[0].textura.width * 2))), (float) (rand() % (janela.height - (inimigos[0].textura.height * 2)))};
+    inimigos[0].hitbox = (Rectangle) {
+        inimigos[0].coordenadas.x + 5,
+        inimigos[0].coordenadas.y,
+        108,
+        165,
+    };
+    return inimigos;
+}
+
 void moverJogador(Player *jogador) {
     if (IsKeyDown(KEY_W)) {
         if (IsKeyDown(KEY_D)) {
-            (*jogador).coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x += GetFrameTime() * (150 * sqrt(2));
         }
         else if (IsKeyDown(KEY_A)) {
-            (*jogador).coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
         }
         else {
-            (*jogador).coordenadas.y -= GetFrameTime() * 300;
+            jogador->coordenadas.y -= GetFrameTime() * 300;
         }  
     }
     else if (IsKeyDown(KEY_S)) {
         if (IsKeyDown(KEY_D)) {
-            (*jogador).coordenadas.y += GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x += GetFrameTime() * (150 * sqrt(2));
         }
         else if (IsKeyDown(KEY_A)) {
-            (*jogador).coordenadas.y += GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
         }
         else {
-            (*jogador).coordenadas.y += GetFrameTime() * 300;
+            jogador->coordenadas.y += GetFrameTime() * 300;
         }  
     }
     else if (IsKeyDown(KEY_A)) {
         if (IsKeyDown(KEY_W)) {
-            (*jogador).coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
         }
         else if (IsKeyDown(KEY_S)) {
-            (*jogador).coordenadas.y += GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x -= GetFrameTime() * (150 * sqrt(2));
         }
         else {
-            (*jogador).coordenadas.x -= GetFrameTime() * 300;
+            jogador->coordenadas.x -= GetFrameTime() * 300;
         } 
     }
     else if (IsKeyDown(KEY_D)) {
         if (IsKeyDown(KEY_W)) {
-            (*jogador).coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y -= GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x += GetFrameTime() * (150 * sqrt(2));
         }
         else if (IsKeyDown(KEY_S)) {
-            (*jogador).coordenadas.y += GetFrameTime() * (150 * sqrt(2));
-            (*jogador).coordenadas.x += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.y += GetFrameTime() * (150 * sqrt(2));
+            jogador->coordenadas.x += GetFrameTime() * (150 * sqrt(2));
         }
         else {
-            (*jogador).coordenadas.x += GetFrameTime() * 300;
+            jogador->coordenadas.x += GetFrameTime() * 300;
         } 
     }
 }
