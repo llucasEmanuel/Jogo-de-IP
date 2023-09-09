0c55206e3a9f16cd329c1652f99fbc9f353b6549
llucasEmanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-09-09 17:54:22 -0300

    Create enemy.h

---
+++ Arquivos temporarios/enemy.h
@@ -0,0 +1,17 @@
+#include <raylib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "player.h"
+
+typedef struct {
+    Texture textura;
+    Vector2 coordenadas;
+    Rectangle hitbox;
+    Vector2 centro;
+    int detectouJogador;
+} Enemy;
+
+Enemy *inicializarInimigos();
+Enemy *atualizarVetorInimigos(Enemy *inimigos, int *qtdInimigos);
+void moveInimigoCirculos(Enemy *inimigo);
+void perseguirJogador(Enemy *inimigo, Player jogador, int qtdInimigos);
