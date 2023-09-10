b795d0b67a49819bcc1183a29574408b698b22cf
manbr21 <136330206+manbr21@users.noreply.github.com>
2023-09-10 04:02:05 -0300

    Update menu.h

--- Bibliotecas/menu.h
+++ Bibliotecas/menu.h
@@ -1,4 +1,21 @@
 #include <raylib.h>
 
-int iniciaJogo();
-int geraMenu();
+typedef struct{
+    Texture BG;
+    int h;
+    int w;
+    int continua;
+    char nome[12];
+    Vector2 mouse;
+} Menu;
+
+typedef struct{
+    Texture w, a, s, d;
+    int continua;
+    
+} Comando;
+
+void iniciaMenu(Menu *menu);
+void iniciaComando(Comando *comando);
+Comando iniciaJogo(Comando comando);
+Menu desenhaMenu(Menu menu);
