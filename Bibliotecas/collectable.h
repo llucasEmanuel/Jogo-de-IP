56148a3da5ae6d30008057ab33e1b8dd0bd008c2
llucasEmanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-09-10 22:43:57 -0300

    Update collectable.h

--- Bibliotecas/collectable.h
+++ Bibliotecas/collectable.h
@@ -9,6 +9,12 @@ typedef struct {
     int colisao;//flag que indica se houve (1) ou nao (0) colisao com aquele coletavel
 } Collectable;//baterias, chave, ...
 
+typedef struct {
+    Texture textura;
+    Vector2 coordenadas;
+} CollectableContador;
+
 Collectable inicializarChave();
 Collectable *inicializarBaterias(int *qtdBaterias);
 Collectable inicializarPorta();
+void InicializaColetavelContador(CollectableContador *mini);
