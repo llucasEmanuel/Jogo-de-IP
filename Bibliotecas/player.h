2334067603e90057c045ca9a406c15296d9d57d3
llucasEmanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-09-17 12:56:48 -0300

    Update player.h

--- Bibliotecas/player.h
+++ Bibliotecas/player.h
@@ -1,7 +1,8 @@
+
 #include <raylib.h>
 
 typedef struct {
-    Texture textura;
+    Texture2D textura;
     Vector2 coordenadas;
     Rectangle hitbox;
     Vector2 centro;
@@ -9,9 +10,13 @@ typedef struct {
     int qtdBaterias;//qtd de baterias coletadas pelo jogador na fase
     int pegouVida;//detecta se o jogador pegou a vida
     int score;
+    float timer;
+    int frame;
+    int maxFrames;
+    float frameLargura;
     float campoVisao;//circulo de luz gerado pela lanterna (varia em funcao do jogador.qtdBaterias)
 } Player;
 
-void moverJogador(Player *jogador);
+void moverJogador(Player *jogador, int numFase);
 Player inicializarJogador();
 void atualizaJogador(Player *jogador);
