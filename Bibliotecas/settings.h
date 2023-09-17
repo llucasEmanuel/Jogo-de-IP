2f40afd7ad04f69491d7458c20f9d0a0a125c71c
llucasEmanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-09-17 12:54:19 -0300

    Update settings.h

--- Bibliotecas/settings.h
+++ Bibliotecas/settings.h
@@ -22,7 +22,7 @@ Camera2D inicializarCamera(Player jogador);
 
 //CARREGAR E ARMAZENAR DADOS
 void carregarMusicas(Music *musicasFase, Music *musicaMenu);
-void carregarSons(Sound *youDied, Sound *musicaPerseguicao, Sound *musicaEncerramento);
+void carregarSons(Sound *youDied, Sound *musicaPerseguicao, Sound *musicaEncerramento, Sound *miau, Sound *somScore, Sound *gameOverSound);
 void salvarScore(char *nome, int score);
 
 //GERAR TELAS
@@ -45,6 +45,10 @@ void gerarHUD(Camera2D camera, Fase fase, int *deathCount, CollectableContador *
 //FUNCOES DE COLISOES
 void removerJogador(Enemy *inimigo, Player *jogador, int *perdeu, int *deathCount, Color cor);
 void pegarChave(Collectable *chave, Player *jogador, Color cor);
-void entrarNaPorta(Collectable *porta, Player *jogador, int *entrouNaPorta, int numFase, Color cor);
+void entrarNaPorta(Collectable *porta, Player *jogador, int *entrouNaPorta, int numFase, Color cor, Sound somScore);
 void pegarBateria(Collectable *bateria, Player *jogador, Color cor);
-void pegarVida(Collectable *vida, Player *jogador, int *deathCount, Color cor, int numFase);
+void pegarVida(Collectable *vida, Player *jogador, int *deathCount, Color cor, int numFase, Sound miau);
+
+//FUNCOES DE CRIAR E RESETAR AS FASES
+void criarFase(int numFase, Fase *fase, Player jogador);
+void reiniciarFase(Player *jogador, Fase *fase);
